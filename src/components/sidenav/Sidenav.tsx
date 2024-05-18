a7a5e70f63555af3f7bcf50d5a149db010d96cf4
Artem Kuznetsov <artemkuz.wmg@gmail.com>
2024-05-18 23:02:17 +0300

    minor changes

--- src/components/sidenav/Sidenav.tsx
+++ src/components/sidenav/Sidenav.tsx
@@ -1,18 +1,20 @@
 import styles from'./Sidenav.module.scss'
 
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import cn from 'classnames'
 import { FoxIcon } from '@/ui/icons/FoxIcon'
 import { routes } from '@/data/routes'
 
 export const Sidenav = ({ animatedLogo }: { animatedLogo: boolean }) => {
+  const pathname = usePathname()
   return (
     <aside className={cn(styles.sidenav, { [styles.animated]: animatedLogo })}>
       <FoxIcon />
       <ul>
         {routes.map(route => (
           <li key={'route_to_' + route.url}>
-            <Link href={route.url} data-alias={route.alias_EN}>
+            <Link href={route.url} data-alias={route.alias_EN} data-active={pathname === route.url}>
               <route.icon />
             </Link>
           </li>
