3119022d947e03860609e2b272411c8432818417
Artem Kuznetsov <artemkuz.wmg@gmail.com>
2024-05-21 19:40:23 +0300

    update sidenav for logout fucntionality

--- src/components/sidenav/Sidenav.tsx
+++ src/components/sidenav/Sidenav.tsx
@@ -3,11 +3,25 @@ import styles from'./Sidenav.module.scss'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import cn from 'classnames'
+import { getCookie, deleteCookie } from 'cookies-next'
+import { LogOut } from 'lucide-react'
 import { FoxIcon } from '@/ui/icons/FoxIcon'
 import { routes } from '@/data/routes'
+import { logout } from '@/utils/api/auth.api'
 
 export const Sidenav = ({ animatedLogo }: { animatedLogo: boolean }) => {
+  const username_cookie = getCookie('username')
   const pathname = usePathname()
+
+  const handleLogout = async () => {
+    const logoutResponse = await logout(username_cookie as string)
+    if (logoutResponse.success) {
+      deleteCookie('username')
+      window.location.reload()
+    }
+    return
+  }
+
   return (
     <aside className={cn(styles.sidenav, { [styles.animated]: animatedLogo })}>
       <FoxIcon />
@@ -20,6 +34,9 @@ export const Sidenav = ({ animatedLogo }: { animatedLogo: boolean }) => {
           </li>
         ))}
       </ul>
+      <footer onClick={handleLogout}>
+        <LogOut />
+      </footer>
     </aside>
   )
 }
